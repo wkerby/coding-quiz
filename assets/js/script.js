3e060477de247b9042a7dd42e490f9eff91b5343
William Kerby <w.kerby99@gmail.com>
2023-01-21 12:29:09 -0500

    create hover effect for answer choices and create initial variables in script.js

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+//create variables
+var hourGlass = document.querySelector("#hourglass");
+
+//create questions, possible answers, and correct answers
+var questions = {
+    Question1: ["What is the language used to apply styling to a webpage?", ["CSS", "JavaScript", "HTML", "C#"], "CSS"],
+    Question2: ["What is the language used to organize contents on a webpage?", ["CSS", "JavaScript", "Ruby", "HTML"], "HTML"],
+    Question3: ["What is the language used to establish interactivity between the end-user and the webpage?", ["Python", "JavaScript", "Perl", "HTML"], "JavaScript"],
+    Question4: ["What object of the window interface allows programmers to store data entered by the end-user?", ["localStorage", "storeObject", "body", "memory"], "LocalStorage"],
+    Question5: ["Is programming fun to learn?", ["No", "No", "Yes", "No"], "Yes"]
+};
